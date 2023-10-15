98cd2e2b807fb9cd44031e4fe44ec3223fcc629f
TPearson420 <pearso420@morrisville.edu>
2023-10-15 04:17:37 -0400

    Getter Methods

--- Quiz-MasterTP/Assets/Scripts/QuestionSO.cs
+++ Quiz-MasterTP/Assets/Scripts/QuestionSO.cs
@@ -6,4 +6,12 @@ public class QuestionSO :ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField]   string question = "Enter new question text here";
+
+
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
+
+
