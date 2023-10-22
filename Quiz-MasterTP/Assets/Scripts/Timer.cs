542786f9b584d48f51872df03ffe42c7522b0f53
TPearson420 <pearso420@morrisville.edu>
2023-10-22 04:44:46 -0400

    Simple Timer

---
+++ Quiz-MasterTP/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    float timerValue;
+
+    public bool isAnsweringQuestion = false;
+    void Update()
+    {
+        UpdateTimer();
+    }
+    
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+        if (isAnsweringQuestion) 
+        {
+            isAnsweringQuestion=false;
+            timerValue = timeToShowCorrectAnswer;
+        
+        }
+        else
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        
+        Debug.Log(timerValue);
+    }
+}
